4efaa1f022c86424f985719e21d51648b29c2043
Visoki <visokie@f8ffc2067a09.local>
2021-09-30 20:00:27 +0300

    add 1 year follow up asthma features

--- data scripts/physicalhealth/physicalhealth.R
+++ data scripts/physicalhealth/physicalhealth.R
@@ -10,5 +10,20 @@ mx01 = load_instrument("abcd_mx01",physicalhealth_files_path)
 mx01 = mx01[,grepl("src|interview|event|sex|(2(a|b|d|g|m)|6(a|l))$",colnames(mx01))]
 
 
-write.csv(file = "outputs/asthma_inflammation_physicalhealth.csv",x = mx01, row.names = F, na = "")
+########### Longitudinal Parent Medical History Questionnaire ########### 
+
+lpmh01 = load_instrument("abcd_lpmh01",physicalhealth_files_path)
+
+#select variables
+lpmh01 = lpmh01[,grepl("src|interview|event|sex|(2(a|b|d|g|m)|6(a|l)_l)",colnames(lpmh01))]
+lpmh01$asthma_composite_l = apply(lpmh01[,c("medhx_2a_l","medhx_6l_l")], 1, function(r) any(r==1)*1)
+
+lpmh01 = lpmh01[lpmh01$eventname == "1_year_follow_up_y_arm_1",]
+
+summary(lpmh01)
+lpmh01[,c("eventname", "interview_date", "interview_age")] = NULL
+
+asthma_inflammation_physicalhealth = merge(mx01,lpmh01, all = T )
+
+write.csv(file = "outputs/asthma_inflammation_physicalhealth.csv",x = asthma_inflammation_physicalhealth, row.names = F, na = "")
 
