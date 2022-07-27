adc6f2e462df8347a26b806ad8f1425fd72a1e10
trant13@chop.edu <trant13@chop.edu>
2022-07-27 11:37:35 -0400

    Fix paths.

--- data scripts/psychopathology/ksad_youth.R
+++ data scripts/psychopathology/ksad_youth.R
@@ -1,9 +1,9 @@
 #note: 2-year follow up: there are new features available that weren't tag yet by ran as internal/external and dsm5
-
+# 2-year Kevin data: not use this
 source("config.R")
 source("utility_fun.R")
 
-ksad_y = load_instrument("abcd_ksad501",psychopathology_files_path)
+ksad_y = load_instrument("abcd_ksad501", abcd_files_path)
 
 #555 and 888 will be treated as NA
 ksad_y[ksad_y == "888" | ksad_y == "555"] = NA
@@ -13,7 +13,7 @@ ksad_y = droplevels(ksad_y)
 
 #################### internal diagnosis #################### 
 #all kids diagnosis
-ksad_y_diagnosis = ksad_y[,grepl("src|inter|event|sex|_((8[3-4][0-9])|863|864|869|870|(91[1-4])|969|970)_t",colnames(ksad_y))]
+ksad_y_diagnosis = ksad_y[, grepl("src|inter|event|sex|_((8[3-4][0-9])|863|864|869|870|(91[1-4])|969|970)_t", colnames(ksad_y))]
 
 #remove empty col
 ksad_y_diagnosis = ksad_y_diagnosis[,!colSums(is.na(ksad_y_diagnosis)) == nrow(ksad_y_diagnosis)]
