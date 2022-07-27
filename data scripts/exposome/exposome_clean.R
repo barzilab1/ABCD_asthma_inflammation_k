150b10625cb0b9bc8429ef69acc9db0566285980
trant13@chop.edu <trant13@chop.edu>
2022-07-27 11:35:58 -0400

    First commit.

--- data scripts/exposome/exposome_clean.R
+++ data scripts/exposome/exposome_clean.R
@@ -4,31 +4,46 @@ source("utility_fun.R")
 
 
 ########### family relationship section ########### 
-acspsw03 = load_instrument("acspsw03",exposome_files_path)
-
-summary(acspsw03)
+acspsw03 = load_instrument("acspsw03", abcd_files_path)
 
 #select variables
-acspsw03 = acspsw03[,grepl("src|interview|event|^sex|rel_family_id",colnames(acspsw03))]
+# acspsw03 = acspsw03[acspsw03$eventname == "baseline_year_1_arm_1",grepl("src|inter|sex|event|fam", colnames(acspsw03))]
+acspsw03 = acspsw03[acspsw03$eventname == "baseline_year_1_arm_1", c("src_subject_id","eventname","rel_family_id")]
 
 
 ########## developmental history ########### 
-dhx01 = load_instrument("dhx01",exposome_files_path)
+dhx01 = load_instrument("dhx01", abcd_files_path)
 
-#select variables
-dhx01 = dhx01[,grepl("src|interview|event|sex|devhx_(3|4)_p$",colnames(dhx01))]
+dhx01[dhx01 == 999 | dhx01 == -1] = NA
+dhx01$accult_select_language = NULL
+
+#change the scale
+dhx01$devhx_caffeine_11 = as.numeric(as.character(dhx01$devhx_caffeine_11))
+dhx01$devhx_caffeine_11[dhx01$devhx_caffeine_11 == 0] = 4
 
 #remove outliers
-dhx01$devhx_3_p = as.numeric(as.character(dhx01$devhx_3_p))
-dhx01$devhx_3_p[dhx01$devhx_3_p > 55] = NA
+dhx01$devhx_11_p = as.numeric(as.character(dhx01$devhx_11_p))
+dhx01$devhx_11_p[dhx01$devhx_11_p > 50] = NA
 
-dhx01$devhx_4_p = as.numeric(as.character(dhx01$devhx_4_p))
-dhx01$devhx_4_p[dhx01$devhx_4_p > 80] = NA
-
-
+dhx01$devhx_16_p = as.numeric(as.character(dhx01$devhx_16_p))
+dhx01$devhx_16_p[dhx01$devhx_16_p > 60] = NA
 
 
-exposome_baseline = merge(acspsw03,dhx01)
+dhx01[,grepl("devhx_19[a-d]_p$", colnames(dhx01))] = apply(dhx01[,grepl("devhx_19[a-d]_p$", colnames(dhx01))], 2, 
+                                                           function(i) as.numeric(as.character(i)))
+dhx01$devhx_19a_p[dhx01$devhx_19a_p > 24] = NA
+dhx01$devhx_19b_p[dhx01$devhx_19b_p < 3 | dhx01$devhx_19b_p > 60] = NA
+dhx01$devhx_19b_p[dhx01$devhx_19c_p < 7 | dhx01$devhx_19c_p > 60] = NA
+dhx01$devhx_19b_p[dhx01$devhx_19d_p < 4 | dhx01$devhx_19d_p > 72] = NA
+#TODO check for each time point
+#remove columns with more than 20% NA
+# dhx01 = dhx01[,-which(colSums(is.na(dhx01)) >= 0.2*dim(dhx01)[1])]
+
+summary(droplevels(dhx01))
+
+
+exposome_baseline = merge(acspsw03, dhx01)
+
 write.csv(file = "outputs/exposome_baseline.csv",x = exposome_baseline, row.names = F, na = "")
 
 
