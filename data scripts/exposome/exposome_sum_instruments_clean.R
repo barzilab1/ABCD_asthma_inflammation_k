5b3a593bfc3c63c2644801e163bf515b14b36e8c
trant13@chop.edu <trant13@chop.edu>
2022-07-27 11:36:24 -0400

    Fix paths.

--- data scripts/exposome/exposome_sum_instruments_clean.R
+++ data scripts/exposome/exposome_sum_instruments_clean.R
@@ -3,29 +3,29 @@ source("config.R")
 source("utility_fun.R")
 
 ########## Sum Scores Culture & Environment Youth ########### 
-sscey01 = load_instrument("abcd_sscey01",exposome_files_path)
+sscey01 = load_instrument("abcd_sscey01", abcd_files_path)
 
 #remove nt (Number Total Questions) and nm (Number Missing Answers) and na (Number Answered)
-sscey01 = sscey01[,!grepl("_(nm|nt|na|answered|pr|dfs)$",colnames(sscey01))] 
+sscey01 = sscey01[,!grepl("_(nm|nt|na|answered|pr|dfs)$", colnames(sscey01))] 
 
 summary(droplevels(sscey01))
 
 # select variables
-sscey01 = sscey01[,grepl("src|interview|event|sex|fes|pmq|crpbi|srpf",colnames(sscey01))]
+sscey01 = sscey01[,grepl("src|interview|event|sex|fes|pmq|crpbi|srpf", colnames(sscey01))]
 
 
 ########### ABCD Sum Scores Mobil Tech Youth ########### 
-ssmty = load_instrument("abcd_ssmty01",exposome_files_path)
+ssmty = load_instrument("abcd_ssmty01", abcd_files_path)
 
 #select variables
-ssmty = ssmty[,grepl("src|interview|event|sex|(weekday|weekend)$",colnames(ssmty))]
+ssmty = ssmty[, grepl("src|interview|event|sex|(weekday|weekend)$", colnames(ssmty))]
 summary(droplevels(ssmty))
 
 
 
 # merge tables
-exposome_sum_baseline = merge(sscey01,ssmty)
-write.csv(file = "outputs/exposome_sum_baseline.csv",x = exposome_sum_baseline, row.names = F, na = "")
+exposome_sum_baseline = merge(sscey01, ssmty)
+write.csv(file = "outputs/exposome_sum_baseline.csv", x = exposome_sum_baseline, row.names = F, na = "")
 
 
 
