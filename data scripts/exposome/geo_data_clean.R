b68fd8c6e1af7a83da951a280f644722796df31d
trant13@chop.edu <trant13@chop.edu>
2022-07-27 11:36:41 -0400

    Use baseline data only.

--- data scripts/exposome/geo_data_clean.R
+++ data scripts/exposome/geo_data_clean.R
@@ -4,16 +4,16 @@ source("utility_fun.R")
 
 
 ########### School Risk and Protective Factors ########### 
-rhds01 = load_instrument("abcd_rhds01",exposome_files_path)
+rhds01 = load_instrument("abcd_rhds01", abcd_files_path)
 
 #select variables
-rhds01 = rhds01[,grepl("^(src|interview|event|sex)|addr1_(popdensity|no2|pm25|proxrd|adi_(income|pov|wsum|perc))$",colnames(rhds01))]
+rhds01 = rhds01[,grepl("^(src|interview|event|sex)|addr1_(popdensity|no2|pm25|proxrd|adi_(income|pov|wsum|perc))$", colnames(rhds01))]
+rhds01 = rhds01[rhds01$eventname == "baseline_year_1_arm_1",]
+rhds01$eventname = NULL
+
+# summary(droplevels(rhds01[rhds01$eventname == "baseline_year_1_arm_1",]))
 
 
-
-summary(droplevels(rhds01[rhds01$eventname == "baseline_year_1_arm_1",]))
-
-
-write.csv(file = "outputs/geo_data_baseline.csv",x = rhds01, row.names = F, na = "")
+write.csv(file = "outputs/geo_data_baseline.csv", x = rhds01, row.names = F, na = "")
 
 
