c17f10e7ad681a3604d8b338bc91ded725688998
trant13@chop.edu <trant13@chop.edu>
2022-07-27 11:49:48 -0400

    Remove unneeded part in this script. Had it when merging.

--- data scripts/exposome/geo_data_clean.R
+++ data scripts/exposome/geo_data_clean.R
@@ -8,8 +8,8 @@ rhds01 = load_instrument("abcd_rhds01", abcd_files_path)
 
 #select variables
 rhds01 = rhds01[,grepl("^(src|interview|event|sex)|addr1_(popdensity|no2|pm25|proxrd|adi_(income|pov|wsum|perc))$", colnames(rhds01))]
-rhds01 = rhds01[rhds01$eventname == "baseline_year_1_arm_1",]
-rhds01$eventname = NULL
+# rhds01 = rhds01[rhds01$eventname == "baseline_year_1_arm_1",]
+# rhds01$eventname = NULL
 
 # summary(droplevels(rhds01[rhds01$eventname == "baseline_year_1_arm_1",]))
 
