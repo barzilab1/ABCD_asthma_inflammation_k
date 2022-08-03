2372a7b9e4223d3c36e30b2d24db4170e05c346c
trant13@chop.edu <trant13@chop.edu>
2022-08-03 14:56:50 -0400

    Add BPM.

--- data scripts/psychopathology/psychopathology_sum_instruments_clean.R
+++ data scripts/psychopathology/psychopathology_sum_instruments_clean.R
@@ -1,11 +1,17 @@
-
 source("config.R")
 source("utility_fun.R")
 
 ################### cbcls ################### 
 cbcls01 = load_instrument("abcd_cbcls01",abcd_files_path)
 
-cbcls = cbcls01[, grepl("^(src|event)|cbcl_scr_syn_totprob_r", colnames(cbcls01))]
+cbcls01 = cbcls01[, grepl("^(src|event)|cbcl_scr_syn_totprob_(r|t)", colnames(cbcls01))]
 
-write.csv(file = "outputs/cbcls.csv",x = cbcls, row.names = F, na = "")
+# write.csv(file = "outputs/cbcls.csv", x = cbcls, row.names = F, na = "")
 
+################### Youth Summary Scores BPM and POA ################### 
+yssbpm01 = load_instrument("abcd_yssbpm01", abcd_files_path)
+yssbpm01 = yssbpm01[,grepl("^(src|interv|event|sex)|bpm_y_scr_totalprob_(r|t)", colnames(yssbpm01))]
+
+psychopathology_sum_scores = merge(cbcls01, yssbpm01)
+
+write.csv(file = "outputs/psychopathology_sum_scores.csv", x = psychopathology_sum_scores, row.names = F, na = "")
